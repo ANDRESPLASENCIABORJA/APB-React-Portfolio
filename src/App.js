2f049ba1b002612678cb78637fac7d3c33a725a1
APB98 <borja.basolutions@gmail.com>
2021-08-02 20:54:24 -0500

    changes in the routes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@
 import React from "react";
 
 // import router
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Import css stylesheet
 import "./App.css";
 import Footer from "./components/Footer";
-
-
 
 //Import files
 import Header from "./components/Header";
@@ -26,22 +24,24 @@ function App() {
       <div className="application-header">
         <Header />
         <div className="container-fluid main-sections">
-          {/* Define routes to render different page components at different paths */}
-          <Route exact path="/">
-            <About />
-          </Route>
-          {/* Define a route that will take in variable data */}
-          <Route exact path="/portfolio">
-            <Portfolio />
-          </Route>
-          {/* Define a route that will take in variable data */}
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-          {/* Define a route that will take in variable data */}
-          <Route exact path="/resume">
-            <Resume />
-          </Route>
+          <Switch>
+            {/* Define routes to render different page components at different paths */}
+            <Route exact path="/APB-React-Portfolio">
+              <About />
+            </Route>
+            {/* Define a route that will take in variable data */}
+            <Route exact path="/portfolio">
+              <Portfolio />
+            </Route>
+            {/* Define a route that will take in variable data */}
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            {/* Define a route that will take in variable data */}
+            <Route exact path="/resume">
+              <Resume />
+            </Route>
+          </Switch>
         </div>
         <Footer />
       </div>
