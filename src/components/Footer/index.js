9d27ba96817404972851bf3d431b1a1f130829c1
APB98 <borja.basolutions@gmail.com>
2021-08-10 02:12:54 -0500

    design changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ import Linkedin from "./linkedin.png";
 const Footer = () => {
   return (
     <footer className = "flex-shrink-0 py-4">
-      <nav className="navbar  navbar-light">
+      <nav className="navbar fixed-bottom navbar-light">
         <div className="container">
           <a href="https://www.linkedin.com/in/andr%C3%A9s-plasencia-borja-120a35ba/">
             <img src={Linkedin} alt="linkedin" width="30" />
